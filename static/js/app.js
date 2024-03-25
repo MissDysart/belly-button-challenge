6c667f6a49afff1389fbc3760e0ebb8e99544105
MissDysart <kelsy.dysart@gmail.com>
2024-03-25 01:00:23 -0600

    initial bar chart works

--- static/js/app.js
+++ static/js/app.js
@@ -5,34 +5,69 @@ const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json"
 d3.json(url).then(function(data) {
   let names = data.names;
   let meta_data = data.metadata;
-  let samples = data.samples;
-//   for (let i = 0; i < length.data; i++) {
-//     console.log(samples[i]);
-//   }
-    // let otuID = samples.id.map(index => index);
-    // console.log(otuID);
+  let sampleSet = data.samples;
+  // Get the top 10 sample values and OTU ids
+  let top10SampleValues = sampleSet.map((item) => item.sample_values.slice(0,10).reverse());
+  let top10otuIDs = sampleSet.map((item) => item.otu_ids.slice(0,10).reverse());
+  let top10otuLabels = sampleSet.map((item) => item.otu_labels.slice(0,10).reverse());
+  
+
+  //let demographics = meta_data.map(row => row.id);
+
+// Initialize visualizations
+
+  function init() {
+    barChart = [{
+        x: top10SampleValues[0],
+        y: top10otuIDs[0].map(item => `OTU ${item}`),
+        text: top10otuLabels[0],
+        type: 'bar',
+        orientation: 'h' }];
+    
+    let layout = {
+        margin: {
+          l: 150,
+          r: 150,
+          t: 50,
+          b: 50
+        }
+    };
+
+    Plotly.newPlot("bar", barChart, layout);
+
+    bubbleChart = [{
+        x: sampleSet.map((item) => item.otu_ids),
+        y: sampleSet.map((item) => item.sample_values),
+        mode: 'markers',
+        marker: {
+            size: sampleSet.map((item) => item.otu_ids)
+        }
+    }];
+    Plotly.newPlot("bubble", bubbleChart);
+  }
+
+//   let info_card = d3.select("#sample-metadata");
+
+init();
 
   // Use D3 to select the dropdown menu
   let dropdownMenu = d3.select("#selDataset");
-  // Bind 'names' to dropdown menu and create option elements in index.html
+  // Bind 'names' to dropdown menu and create option elements in index.html (from Xpert Learning Assistant)
   let options = dropdownMenu.selectAll("option")
     .data(names)
     .enter()
     .append("option");
   options.text(n => n).attr("value", n=>n);
 
-// Initialize visualizations
+  // Add an onchange event listener to the dropdown menu (from Xpert Learning Assistant)
+  dropdownMenu.on("change", function() {
+    let selectedValue = d3.select(this).property("value");
+    optionChanged(selectedValue);
+    console.log(selectedValue.text());
+  });
 
-    // function init() {
-    //     dataPlot = [{
-    //         x: ,
-    //         y: [1, 2, 4, 8, 16],
-    //         type: 'bar',
-    //         orientation: 'h' }];
-  
-    //     Plotly.newPlot("bar", data);
-    // }
-    // init();
+
+
 });
 
 
